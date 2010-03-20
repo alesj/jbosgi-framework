34ea18e7a423b81d5d9028cf22f78983b78e3913
thomas.diesler@jboss.com <thomas.diesler@jboss.com@84be2c1e-ba19-0410-b317-a758671a6fc1>
2010-03-20 01:03:36 +0000

    Use explicit manifest.
    git-svn-id: https://svn.jboss.org/repos/jbossas/projects/jboss-osgi/projects/runtime/framework/trunk@102655 84be2c1e-ba19-0410-b317-a758671a6fc1

--- bundle/src/test/java/org/jboss/test/osgi/launch/FrameworkLaunchTestCase.java
+++ bundle/src/test/java/org/jboss/test/osgi/launch/FrameworkLaunchTestCase.java
@@ -25,8 +25,17 @@ package org.jboss.test.osgi.launch;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Map;
+
+import org.jboss.osgi.framework.launch.OSGiFrameworkFactory;
 import org.jboss.osgi.spi.util.ConstantsHelper;
 import org.jboss.osgi.spi.util.ServiceLoader;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.osgi.framework.BundleException;
 import org.osgi.framework.launch.Framework;
@@ -38,28 +47,58 @@ import org.osgi.framework.launch.FrameworkFactory;
  * @author thomas.diesler@jboss.com
  * @since 27-Jul-2009
  */
-public class FrameworkLaunchTestCase 
+public class FrameworkLaunchTestCase
 {
    @Test
    public void testFrameworkLaunch() throws BundleException
    {
       FrameworkFactory factory = ServiceLoader.loadService(FrameworkFactory.class);
       Framework framework = factory.newFramework(null);
-      
+
       assertEquals("BundleId == 0", 0, framework.getBundleId());
       assertEquals("SymbolicName", "system.bundle", framework.getSymbolicName());
-      
+
       String state = ConstantsHelper.bundleState(framework.getState());
       assertEquals("INSTALLED", state);
-      
+
       framework.init();
-      
+
       state = ConstantsHelper.bundleState(framework.getState());
       assertEquals("STARTING", state);
-      
+
       framework.start();
-      
+
       state = ConstantsHelper.bundleState(framework.getState());
       assertEquals("ACTIVE", state);
+   }
+
+   @Ignore
+   public void testFrameworkAllLaunch() throws Exception
+   {
+      // Get the aggregated framework jar
+      File[] files = new File("./target").listFiles(new FilenameFilter()
+      {
+         public boolean accept(File dir, String name)
+         {
+            return name.startsWith("jboss-osgi-framework-") && name.endsWith("-all.jar");
+         }
+      });
+      assertEquals(1, files.length);
+
+      // Use a classloader that only contains the aggregated framework jar
+      URL frameworkAllURL = files[0].toURI().toURL();
+      URLClassLoader loader = new URLClassLoader(new URL[] { frameworkAllURL }, null);
+
+      // Load the FrameworkFactory
+      Class<?> factoryClass = loader.loadClass(OSGiFrameworkFactory.class.getName());
+      Object frameworkFactory = factoryClass.newInstance();
+
+      // Construct the Framework
+      Method method = factoryClass.getMethod("newFramework", Map.class);
+      Object framework = method.invoke(frameworkFactory, new Object[] { null });
+
+      // Start the Framework
+      method = framework.getClass().getMethod("start", new Class[] {});
+      method.invoke(framework, new Object[] {});
    }
 }
